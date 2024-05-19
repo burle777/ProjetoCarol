ab11e133be6d35934df528801a61d26e464fa572
GuilhermeRangelSilva <162377684+GuilhermeRangelSilva@users.noreply.github.com>
2024-05-19 16:47:57 -0300

    Update Código.py
    versão do código que estava com tratamento de erro e estava sem a função favoritos, agora com essa função implementada

--- Código.py
+++ Código.py
@@ -4,10 +4,8 @@ import random
 
 arquivo_receitas = "receitas.csv"
 
-
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
-    
 
 def menu_principal():
     while True:
@@ -27,6 +25,7 @@ def menu_principal():
         print("5 - Filtrar Receitas Por País")
         print("6 - Sugestão de Receita Aleatória")
         print("7 - Escolher receita por ingredientes")
+        print("8 - Adicionar Receita aos Favoritos")
         print("0 - Sair")
         
         escolha = input("Escolha uma opção: ")
@@ -45,21 +44,22 @@ def menu_principal():
             receita_aleatoria()
         elif escolha == '7':
             buscar_por_ingredientes()
+        elif escolha == '8':
+            fav()
         elif escolha == '0':
             print("Saindo do sistema...")
             break
         else:
             print("Opção inválida! Por favor, tente novamente.")
         
-        input("Pressione Enter para continuar...")    
-
+        input("Pressione Enter para continuar...")
 
 def adicionar_receita():
     nome = input("Nome da receita: ")
     pais = input("País de origem: ")
     ingredientes = input("Ingredientes (separados por vírgula): ")
     modo_preparo = input("Modo de preparo: ")
-    nova_receita = [nome, pais, ingredientes, modo_preparo]
+    nova_receita = [nome, pais, ingredientes, modo_preparo, "False"]
     
     try:
         with open(arquivo_receitas, 'a', newline='', encoding='utf-8') as f:
@@ -70,16 +70,14 @@ def adicionar_receita():
     except Exception:
         print("Erro ao adicionar receita!\n")
 
-
 def visualizar_receitas():
     try:
         with open(arquivo_receitas, 'r', newline='', encoding='utf-8') as f:
             reader = csv.reader(f)
             for i, row in enumerate(reader, 1):
-                print(f"{i}. Nome: {row[0]}, País: {row[1]}, Ingredientes: {row[2]}, Modo de Preparo: {row[3]}")
+                print(f"{i}. Nome: {row[0]}, País: {row[1]}, Ingredientes: {row[2]}, Modo de Preparo: {row[3]}, Favorito: {row[4]}")
     except FileNotFoundError:
         print("Ainda não há receitas cadastradas.\n")
-
 
 def atualizar_receita():
     visualizar_receitas()
@@ -114,11 +112,8 @@ def atualizar_receita():
 
     except ValueError:
         print("Entrada inválida. Por favor, digite um número.\n")
-
     else:
         print("Número da receita inválido.\n")
-
-
 
 def deletar_receita():
     visualizar_receitas()
@@ -138,13 +133,11 @@ def deletar_receita():
                 writer.writerows(receitas)
             
             print("Receita deletada com sucesso!\n")
-
         else:
             print("Número da receita inválido.\n")
     
     except ValueError:
-        print("Entrada inválida. Por favor, digite um número. \n")
-
+        print("Entrada inválida. Por favor, digite um número.\n")
 
 def filtrar_por_pais():
     pais_desejado = input("Digite o país cujas receitas você deseja visualizar: ").lower()
@@ -155,7 +148,7 @@ def filtrar_por_pais():
             reader = csv.reader(f)
             for row in reader:
                 if row[1].lower() == pais_desejado:
-                    print(f"Nome: {row[0]}, Ingredientes: {row[2]}, Modo de Preparo: {row[3]}")
+                    print(f"Nome: {row[0]}, Ingredientes: {row[2]}, Modo de Preparo: {row[3]}, Favorito: {row[4]}")
                     encontrou = True
         
         if not encontrou:
@@ -164,7 +157,6 @@ def filtrar_por_pais():
         print("Ainda não há nenhuma receita cadastrada!\n")
     except Exception:
         print("Ocorreu um erro ao ler as receitas!\n")
-
 
 def receita_aleatoria():
     try:
@@ -177,11 +169,11 @@ def receita_aleatoria():
             print(f'País: {receita_aleatoria[1]}')
             print(f'Ingredientes: {receita_aleatoria[2]}')
             print(f'Modo de preparo: {receita_aleatoria[3]}')
+            print(f'Favorito: {receita_aleatoria[4]}')
     except FileNotFoundError:
         print("Ainda não há receitas cadastradas.\n")
     except Exception:
         print("Erro ao adicionar uma receita aleatória!\n")
-
 
 def buscar_por_ingredientes():
     ingredientes_desejados = set(input("Digite os ingredientes desejados (separados por vírgula): ").split(", "))
@@ -192,11 +184,39 @@ def buscar_por_ingredientes():
         for row in reader:
             ingredientes_receita = set(row[2].split(", "))
             if ingredientes_desejados.issubset(ingredientes_receita):
-                print(f"Nome: {row[0]}, País: {row[1]}, Modo de Preparo: {row[3]}")
+                print(f"Nome: {row[0]}, País: {row[1]}, Modo de Preparo: {row[3]}, Favorito: {row[4]}")
                 encontrou = True
     
     if not encontrou:
         print("Não foram encontradas receitas com os ingredientes informados.\n")
+
+def fav():
+    nome_receita = input("Digite o nome da receita que deseja adicionar aos favoritos: ")
+    receitas = []
+    
+    try:
+        with open(arquivo_receitas, 'r', newline='', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            receitas = list(reader)
+        
+        encontrou = False
+        for receita in receitas:
+            if receita[0].lower() == nome_receita.lower():
+                receita[4] = "True"
+                encontrou = True
+                break
+        
+        if encontrou:
+            with open(arquivo_receitas, 'w', newline='', encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writerows(receitas)
+            print("Receita adicionada aos favoritos com sucesso!\n")
+        else:
+            print("Receita não encontrada.\n")
+    except FileNotFoundError:
+        print("Ainda não há receitas cadastradas.\n")
+    except Exception:
+        print("Erro ao adicionar receita aos favoritos!\n")
 
 if __name__ == "__main__":
     menu_principal()
